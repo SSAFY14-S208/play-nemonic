760c6c26c9545d92868a11e578f25df01e6120b7
손다현 <sonda2356@naver.com>
2026-04-21 10:40:31 +0900

    [BE] chore: CORS 설정 추가

--- backend/src/main/java/com/nemonicworld/config/SecurityConfig.java
+++ backend/src/main/java/com/nemonicworld/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nemonicworld.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,7 +14,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
         return http.build();
     }
 }
