f75f50b4990bd411485359a9d9549eb1bb302e3a
LeeTaeHee <k58701@naver.com>
2026-04-30 11:00:23 +0900

    [BE] feat: 익명 사용자 발급 API 추가

---
+++ backend/src/main/java/com/nemonicworld/user/api/UserController.java
@@ -0,0 +1,34 @@
+package com.nemonicworld.user.api;
+
+import com.nemonicworld.common.response.ApiResponse;
+import com.nemonicworld.user.application.UserService;
+import com.nemonicworld.user.dto.response.AnonymousUserResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private static final String ANONYMOUS_USER_CREATED_MESSAGE = "익명 사용자 UUID 발급 성공";
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/anonymous")
+    public ResponseEntity<ApiResponse<AnonymousUserResponse>> createAnonymousUser(
+        @RequestHeader(value = HttpHeaders.USER_AGENT, required = false) String userAgent) {
+        AnonymousUserResponse response = userService.createAnonymousUser(userAgent);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(ANONYMOUS_USER_CREATED_MESSAGE, response));
+    }
+}
