59d73f711e4a4d479c9349979dd484ae7ae21171
손다현 <sonda2356@naver.com>
2026-05-07 09:15:38 +0900

    [BE] refactor: 공통 설정 클래스를 global config로 이동

--- backend/src/main/java/com/nemonicworld/auth/controller/AuthController.java
+++ backend/src/main/java/com/nemonicworld/auth/controller/AuthController.java
@@ -10,7 +10,7 @@ import com.nemonicworld.auth.service.AuthService;
 import com.nemonicworld.common.jwt.AdminPrincipal;
 import com.nemonicworld.common.openapi.OpenApiErrorExamples;
 import com.nemonicworld.common.response.ApiResponse;
-import com.nemonicworld.config.OpenApiConfig;
+import com.nemonicworld.global.config.OpenApiConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
--- backend/src/main/java/com/nemonicworld/global/config/OpenApiConfig.java
+++ backend/src/main/java/com/nemonicworld/global/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.nemonicworld.config;
+package com.nemonicworld.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
