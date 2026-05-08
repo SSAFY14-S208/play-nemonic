78bd3a10d5f0780eb2d5b39bd04a265c4347f0ed
안지원 <wl990@naver.com>
2026-05-08 10:14:42 +0900

    [FE] feat: nextConfig image 도메인 주소 등록 관련 주석 작성

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 
   reactStrictMode: false,
-
+  // Next.js의 <Image> 컴포넌트는 외부 도메인 이미지를 사용할 때 remotePatterns에 해당 호스트를 등록해야 함
   images: {
     remotePatterns: [
       {
