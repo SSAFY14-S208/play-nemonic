f1e1bccceafa77a651afa987fc38a0cc3e70e088
hyebinle <qnd1215@naver.com>
2026-05-18 09:26:19 +0900

    [FE] fix: 플립북 모바일 입장 로고

--- frontend/src/app/(service)/(game)/flipbook/page.tsx
+++ frontend/src/app/(service)/(game)/flipbook/page.tsx
@@ -1,9 +1,5 @@
 import { WorldHomeLink } from '@/shared/components'
 
 export default function Page() {
-  return (
-    <div className="hidden sm:block">
-      <WorldHomeLink />
-    </div>
-  )
+  return <WorldHomeLink />
 }
